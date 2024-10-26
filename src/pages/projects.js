368425ffa7215d2ce39e44e94fec4430f765e672
xalimjonovxojimurod <158495568+xalimjonovxojimurod@users.noreply.github.com>
2024-10-26 18:58:48 +0500

    Add files via upload

---
+++ src/pages/projects.js
@@ -0,0 +1,211 @@
+import AnimatedText from '@/components/AnimatedText'
+import { GithubIcon } from '@/components/Icons'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import {motion} from "framer-motion"
+import TransitionEffect from '@/components/TransitionEffect'
+
+const FramerImage = motion(Image);
+
+
+const FeaturedProject = ({type, title, summary, img, link, github}) => {
+    return (
+      <article className='w-full flex items-center justify-between relative  rounded-br-2xl
+        rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light 
+         lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
+        '>
+        
+        <div className=' absolute  top-0  -right-3  -z-10  w-[101%]  h-[103%]  rounded-[2.5rem]  bg-dark  rounded-br-3xl dark:bg-light
+        xs:-right-2 xs:h-[102%] sm:w-full xs:rounded-[1.5rem]
+        '/>
+
+        {/* Project Image */}
+        <Link href={link} target="_blank" 
+          className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full' 
+          aria-label={`Link to ${title} project`}>
+          <FramerImage src={img} alt={title} className='w-full h-auto'
+                whileHover={{scale:1.05}}
+                transition={{duration:0.2}}
+                priority
+                 sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
+                 />
+        </Link>
+        
+        {/* Project Details */}
+        <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+          <span className='text-primary dark:text-primaryDark font-medium text-xl xs:text-base'>{type}</span>
+          
+          <Link href={link} target="_blank" 
+            className='hover:underline underline-offset-2' aria-label={`Visit ${title}`}>
+            <h2 className='my-2 w-full text-left text-3xl md:text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>
+          </Link>
+          
+          <p className='my-1 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
+          
+          <div className='flex items-center mt-4 space-x-4'>
+            <Link href={github} target="_blank" aria-label={`GitHub repository for ${title}`}>
+              <GithubIcon className='w-6 h-6' />
+            </Link>
+            
+            <Link href={link} target="_blank" className='text-primary dark:text-primaryDark font-semibold hover:underline sm:px-4 sm:text-base'>
+              Visit Project
+            </Link>
+          </div>
+        </div>
+      </article>
+    );
+  };
+  
+
+const Project = ({type, title, img, link, github}) =>{
+    return(
+        <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid
+        border-dark bg-light p-6 relative dark:border-light dark:bg-dark xs:p-4'>
+
+<div className=' absolute  top-0  -right-3  -z-10  w-[101%]  h-[103%]  rounded-[2rem]  bg-dark dark:bg-light 
+rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'
+/>
+            
+            {/* Project Image */}
+        <Link href={link} target="_blank" 
+          className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full '>
+          <FramerImage src={img} alt={title} className='w-full h-auto'
+                whileHover={{scale:1.05}}
+                transition={{duration:0.2}}
+                priority
+                 sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
+                 />
+        </Link>
+        
+        {/* Project Details */}
+        <div className='w-full flex flex-col items-start justify-between mt-4'>
+          <span className='text-primary dark:text-primaryDark font-medium text-lg lg:text-lg md:text-base '>{type}</span>
+          
+          <Link href={link} target="_blank" 
+            className='hover:underline underline-offset-2' >
+            <h2 className='my-2 w-full text-left text-3xl font-bold text-dark dark:text-light lg:text-2xl'>{title}</h2>
+          </Link>
+          
+          <div className='w-full mt-2 flex items-center justify-between'>
+
+          <Link href={link} target="_blank" className='text-primary dark:text-primaryDark font-semibold hover:underline sm:px-4 sm:text-base'>
+              Visit
+            </Link>
+          
+            <Link href={github} target="_blank" className='w-8 md:w-6'>
+              <GithubIcon  />
+            </Link>
+            
+            
+          </div>
+        </div>
+        </article>
+    )
+}
+
+
+
+
+const projects = () => {
+  return (
+    <>
+
+         <Head>
+             <title>CodeBucks | About Page</title>
+              <meta name="description" content="any description" />
+         </Head>
+
+         <TransitionEffect />
+  
+         <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
+            <Layout className='pt-16'>
+                <AnimatedText text="Imagination Trumps Knowledge!" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'/>
+
+                <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
+                    <div className='col-span-12'>
+                        <FeaturedProject 
+                          title="Crypto Screener Application"
+                          img={project1}
+                          summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                          React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                          You can easily convert the price in your local currency."
+                           link="/"
+                           github="/"
+                           type=" Featured Project"                           
+                        />
+                    </div>
+                    <div className='col-span-6 sm:col-span-12'>  {/* Project qo'shish  col-span-6 */}
+                       
+                        <Project 
+                        title="Crypto Screener Application"
+                        img={project1}
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                        React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                        You can easily convert the price in your local currency."
+                         link="/"
+                         github="/"
+                         type=" Featured Project"
+                        />
+                    </div>
+                    <div className='col-span-6 sm:col-span-12'>
+                    <Project 
+                        title="Crypto Screener Application"
+                        img={project1}
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                        React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                        You can easily convert the price in your local currency."
+                         link="/"
+                         github="/"
+                         type=" Featured Project"
+                        />
+                    </div>
+
+                    <div className='col-span-12'>
+                    <FeaturedProject 
+                          title="Crypto Screener Application"
+                          img={project1}
+                          summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                          React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                          You can easily convert the price in your local currency."
+                           link="/"
+                           github="/"
+                           type=" Featured Project"                           
+                        />
+                    </div>
+                    <div className='col-span-6 sm:col-span-12'>
+                    <Project 
+                        title="Crypto Screener Application"
+                        img={project1}
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                        React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                        You can easily convert the price in your local currency."
+                         link="/"
+                         github="/"
+                         type=" Featured Project"
+                        />
+                    </div>
+                    <div className='col-span-6 sm:col-span-12'>
+                    <Project 
+                        title="Crypto Screener Application"
+                        img={project1}
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, 
+                        React Router and Recharts. It shows detail regarding almost all the cryptocurrency. 
+                        You can easily convert the price in your local currency."
+                         link="/"
+                         github="/"
+                         type=" Featured Project"
+                        />
+                    </div>
+                </div>
+            </Layout>
+         </main>
+
+    </>
+  )
+}
+
+export default projects  //4:06:45 npm run dev
